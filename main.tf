06c255badae40d8391975a3adf767f53e13b8f24
mykwan127 <mykwan0127@gmail.com>
2021-08-26 20:09:49 +0900

    sg add

--- main.tf
+++ main.tf
@@ -3,10 +3,27 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-	ami  = "ami-0a0de518b1fc4524c"
+	ami  = "ami-0ba5cd124d7a79612"
 	instance_type = "t2.micro"
-    
+    vpc_security_group_ids = [aws_security_group.instance1.id]
+
+    user_data = <<-EOF
+                    #!/bin/bash
+                    echo "Hello, terraform world!!" > index.html
+                    nohup busybox httpd -f -p 8080 &
+                    EOF
     tags = {
         Name = "terraform-example"
+    }
+}
+
+resource "aws_security_group" "instance1" {
+    name = "terraform-example-instance"
+
+    ingress {
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
     }
 }
